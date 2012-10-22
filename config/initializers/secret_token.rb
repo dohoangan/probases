1620d28a7ec2e590a6de0c417b8e73a652ddcf02
An D. Hoang <an@izproperty.com>
2012-10-22 16:50:33 +0700

    Innititalize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Probases::Application.config.secret_token = '91cfa5e477308d1c1ca10180a1c0b3ede36918f19b5bf637cf333f5333197ccac555da92dda2af559cb2f450074a0fdc926159d447684a39714dc69e85ce7fa9'
