43517d2e5725ad24b06a993180ce91ab2008415a
An D. Hoang <an@izproperty.com>
2012-10-26 05:41:47 +0700

    Finish Authenticate with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Probases::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Config mailer host
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
