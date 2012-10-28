c1eebfb6b02fc2078fc8886a2e27b2d045dcad90
An D. Hoang <an@izproperty.com>
2012-10-28 12:22:18 +0700

    Bootstrap

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+  	@user = User.new(email: "an@example.org", password: "foobar", password_confirmation: "foobar")
+  end
+
+  subject { @user }
+
+	it { should respond_to :email }  
 end
